75ac7b402b0a09cd9f9d3831591d75d256137039
yingjun <yingjun@comp.nus.edu.sg>
2014-05-03 22:48:54 +0800

    add virtualization

--- virtualization/client.cpp
+++ virtualization/client.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <cstring>
+#include <string>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 #include "../tools/time_measurer.h"
 
 char *state;
-const int kTupleNum=10000;
+const int kTupleNum=100000000;
 TimeMeasurer timer;
 
 void CreateState(){
 	state=(char*)malloc(kTupleNum*sizeof(int));
 	memset(state, 0, kTupleNum*sizeof(int));
+}
+
+void GetMMapState(){
+	int fd=open("foo", O_RDWR);
+	int filesize=kTupleNum*sizeof(int);
+	state=(char*)mmap(NULL, filesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	memset(state, 0, filesize);
 }
 
 void WriteState(const int &position, const int &number){
@@ -20,7 +34,7 @@ void ReadState(const int &position, int &number){
 	memcpy(&number, state+position*sizeof(int), sizeof(int));
 }
 
-void WriteProcessRround(const int &round){
+void WriteProcessRound(const int &round){
 	int total=0;
 	for(int r=0; r<round; ++r){
 		for(int i=0; i<kTupleNum; ++i){
@@ -37,12 +51,23 @@ void WriteSnapshot(const std::string &filename){
 }
 
 int main(){
+	//CreateState();
+	GetMMapState();
+	std::cout<<"mmap state obtained!"<<std::endl;
+	timer.StartTimer();
+	WriteProcessRound(10);
+	timer.EndTimer();
+	std::cout<<"state size="<<kTupleNum*sizeof(int)/1024/1024<<"MB"<<std::endl;
+	std::cout<<"elapsed time="<<timer.GetElapsedMilliSeconds()<<"ms"<<std::endl;
+
 	CreateState();
 	timer.StartTimer();
-	WriteProcessRound(1000);
+	WriteProcessRound(10);
 	timer.EndTimer();
+	std::cout<<"state size="<<kTupleNum*sizeof(int)/1024/1024<<"MB"<<std::endl;
 	std::cout<<"elapsed time="<<timer.GetElapsedMilliSeconds()<<"ms"<<std::endl;
-	WriteSnapshot("snapshot.dat");
+	//WriteSnapshot("snapshot.dat");
+	getchar();
 	free(state);
 	return 0;
 }
--- virtualization/testclient.cpp
+++ virtualization/testclient.cpp
@@ -10,17 +10,14 @@
 
 const int kTupleNum=100000000;
 int main(){
-	int fd=open("foo", O_RDWR|O_CREAT|O_TRUNC);
+	int fd=open("foo", O_RDWR);
 	int filesize=kTupleNum*sizeof(int);
-	lseek(fd, filesize-1, SEEK_SET);
-	write(fd, "", 1);
 	char *data=(char*)mmap(NULL, filesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	memset(data, 0, filesize);
-	int value=23;
-	memcpy(data, &value, sizeof(value));
+	int value;
+	memcpy(&value, data, sizeof(value));
+	std::cout<<"get value="<<value<<std::endl;
 	getchar();
 	munmap(data, filesize);
 	close(fd);
-	std::cout<<"closed!"<<std::endl;
 	return 0;
 }
