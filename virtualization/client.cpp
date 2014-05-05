72354a8a8284c359ebdf7d8a51b15a3ad795cb87
yingjun <wu.yj0616@gmail.com>
2014-05-06 00:25:32 +0800

    update

--- virtualization/client.cpp
+++ virtualization/client.cpp
@@ -4,27 +4,47 @@
 #include <cstring>
 #include <string>
 #include <ctime>
+#include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <sys/mman.h>
 #include "../tools/time_measurer.h"
 
 char *state;
 const int kTupleNum=100000000;
+const int kFileSize=kTupleNum*sizeof(int);
 TimeMeasurer timer;
 
 void CreateState(){
-	state=(char*)malloc(kTupleNum*sizeof(int));
-	memset(state, 0, kTupleNum*sizeof(int));
+	state=(char*)malloc(kFileSize);
+	memset(state, 0, kFileSize);
+}
+
+void GetHypervisorState(){
+	int fd=open("foo", O_RDWR);
+	state=(char*)mmap(NULL, kFileSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	memset(state, 0, kFileSize);
 }
 
 void GetMMapState(){
-	int fd=open("foo", O_RDWR);
-	int filesize=kTupleNum*sizeof(int);
-	state=(char*)mmap(NULL, filesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	memset(state, 0, filesize);
+	int fd=open("foo", O_RDWR|O_CREAT|O_TRUNC, (mode_t)0600);
+	lseek(fd, kFileSize-1, SEEK_SET);
+	write(fd, "", 1);
+	state=(char*)mmap(NULL, kFileSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	memset(state, 0, kFileSize);
+}
+
+void GetAnonState(){
+	state=(char*)mmap(NULL, kFileSize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
+	memset(state, 0, kFileSize);
+}
+
+void GetShmState(){
+	int fd=shm_open("shm", O_RDWR|O_CREAT|O_TRUNC, (mode_t)0600);
+	ftruncate(fd, kFileSize);
+	state=(char*)mmap(NULL, kFileSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	memset(state, 0, kFileSize);
 }
 
 void WriteState(const int &position, const int &number){
@@ -61,7 +81,7 @@ void WriteProcessRoundRandom(const int &round){
 
 void WriteSnapshot(const std::string &filename){
 	std::ofstream outfile(filename, std::ofstream::binary);
-	outfile.write(state, kTupleNum*sizeof(int));
+	outfile.write(state, kFileSize);
 	outfile.close();
 }
 
@@ -69,27 +89,27 @@ void Process(){
 	timer.StartTimer();
 	WriteProcessRound(100);
 	timer.EndTimer();
-	std::cout<<"state size="<<kTupleNum*sizeof(int)/1024/1024<<"MB"<<std::endl;
+	std::cout<<"state size="<<kFileSize/1024/1024<<"MB"<<std::endl;
 	std::cout<<"elapsed time="<<timer.GetElapsedMilliSeconds()<<"ms"<<std::endl;
 }
 
 void ProcessRandom(){
 	timer.StartTimer();
-	WriteProcessRoundRandom(2);
+	WriteProcessRoundRandom(10);
 	timer.EndTimer();
-	std::cout<<"state size="<<kTupleNum*sizeof(int)/1024/1024<<"MB"<<std::endl;
+	std::cout<<"state size="<<kFileSize/1024/1024<<"MB"<<std::endl;
 	std::cout<<"elapsed time="<<timer.GetElapsedMilliSeconds()<<"ms"<<std::endl;
 }
 
 int main(){
 	srand(time(0));
+//	GetMMapState();
+	GetShmState();
+	Process();
+	ProcessRandom();
 	CreateState();
 	Process();
 	ProcessRandom();
-	GetMMapState();
-	Process();
-	ProcessRandom();
-	getchar();
 	free(state);
 	return 0;
 }
--- virtualization/hypervisor.cpp
+++ virtualization/hypervisor.cpp
@@ -8,17 +8,14 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 
-const int kTupleNum=100000000;
+const int kTupleNum=1000000;
 int main(){
-	int fd=open("foo", O_RDWR|O_CREAT|O_TRUNC);
+	int fd=open("foo", O_RDWR|O_CREAT|O_TRUNC, (mode_t)0600);
 	int filesize=kTupleNum*sizeof(int);
 	lseek(fd, filesize-1, SEEK_SET);
 	write(fd, "", 1);
 	char *data=(char*)mmap(NULL, filesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	memset(data, 0, filesize);
-	int value=23;
-	memcpy(data, &value, sizeof(value));
-	getchar();
 	munmap(data, filesize);
 	close(fd);
 	std::cout<<"closed!"<<std::endl;
